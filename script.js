9ca8211bba4b0f81cb82363b6e3734a3a8b36ae1
c <thomascavalcante77@gmail.com>
2022-05-25 20:23:44 -0400

    Updates

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const context = canvas.getContext("2d");
 
 const frameCount = 50;
 const currentFrame = index => (
-  `/dist/${index.toString().padStart()}.jpg`
+  `${index.toString().padStart()}.jpg`
 )
 
 const preloadImages = () => {
